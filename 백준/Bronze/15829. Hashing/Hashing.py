8c08f8483652043b1c2159dce296aa26423c7b87
서민재 <110338700+Vak-kas@users.noreply.github.com>
2023-06-23 14:19:36 +0900

    Update Hashing.py

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -5,10 +5,13 @@ string = sys.stdin.readline().rstrip();
 
 degree = 0;
 value = 0;
+M = 1234567891;
 for i in string:
     a = ord(i)-96;
     value += 31**degree * a;
     degree+=1;
+    
+value %=M;
     
 print(value);
 
