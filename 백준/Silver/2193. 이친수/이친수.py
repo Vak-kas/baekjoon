9c2a4c400c7ffda59c50bb3f6dbf509a771e50f5
서민재 <110338700+Vak-kas@users.noreply.github.com>
2023-07-12 17:04:51 +0900

    [Silver III] Title: 이친수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -0,0 +1,12 @@
+import sys;
+
+n = int(sys.stdin.readline().rstrip());
+memo = [0, 1, 1];
+
+if n<2:
+    print(memo[n]);
+else:
+    for i in range(2, n):
+        memo.append(memo[-1] + memo[-2])
+        
+    print(memo[-1])
