9509ff042cc010282b532311e1cbcc7258307dab
서민재 <110338700+Vak-kas@users.noreply.github.com>
2023-09-13 23:20:55 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,22 +1,18 @@
-a = [];
-b = [];
-for i in range(1, 31):
-    a.append(i);
+num1 = set([i for i in range(1, 31)]) #1부터 30까지를 집합으로 넣기
+num2 = set([]); # 공집합 만들기
+
+for i in range(28):
+    a = int(input()); #값을 입력받고
+    num2.add(a);    # 공집합에 넣기
     
-for j in range(28):
-    k = int(input());
-    b.append(k)
-        
     
-m = set(a);
-n = set(b);
+num3 = list(num1-num2); #차집합을 구한 후, 리스트로 변환하여 num3에 넣기
 
-l = list(m-n);
+num3.sort(); #num3 정렬하기
 
-l.sort();
-
-print(l[0]);
-print(l[1]);
+for i in num3:
+    print(i);   #num3 출력하기
+    
 
 
     
