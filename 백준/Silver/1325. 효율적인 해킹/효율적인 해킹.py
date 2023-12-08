73a51350178c026c71a1b5b1599845d89d6b85e4
서민재 <110338700+Vak-kas@users.noreply.github.com>
2023-12-08 18:05:42 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10420 ms, Memory: 210888 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -1,54 +1,33 @@
+from collections import deque
 import sys
-
-def printEdgeList(array):
-    for i in range(1, len(array)):
-        print(f"{i} --> {array[i]}")
-
-
-def BFS(x):
-    global edgeList, ans;
-    from collections import deque;
-    visitedAry = [False for _ in range(node+1)];
-    queue = deque();
-    current = x;
-    queue.append(current)
-    visitedAry[current] = True
-    while queue:
-        current = queue.popleft();
-        
-        for i in edgeList[current]:
-            if not visitedAry[i]:
-                queue.append(i);
-                visitedAry[i] = True;
-                ans[x-1] +=1;
-
-        
-    
-    
-
-node, repeat = map(int, sys.stdin.readline().split());
-edgeList = [[] for _ in range(node+1)];
-for i in range(repeat):
-    a, b = map(int, sys.stdin.readline().split());
-    edgeList[b].append(a);
-    
-    
-
-    
-# printEdgeList(edgeList)
-ans= [0 for _ in range(node)]
-for i in range(1, node+1):
-    BFS(i);
-
-    
-maxVal = 0;
-for i in range(node):
-    maxVal = max(maxVal, ans[i]);
-
-for i in range(len(ans)):
-    if maxVal == ans[i]:
-        print(f"{i+1}", end=" ");
-    
-    
-    
-    
+input = sys.stdin.readline
+def bfs(start):
+    q = deque()
+    q.append(start)
+    visit = [0 for _ in range(n + 1)]
+    visit[start] = 1
+    cnt = 1
+    while q:
+        st = q.popleft()
+        for i in s[st]:
+            if visit[i] == 0:
+                visit[i] = 1
+                cnt += 1
+                q.append(i)
+    return cnt
+n, m = map(int, input().split())
+s = [[] for i in range(n + 1)]
+for i in range(m):
+    a, b = map(int, input().split())
+    s[b].append(a)
+result = []
+max_cnt = 0
+for i in range(1, n + 1):
+    tmp = bfs(i)
+    if max_cnt == tmp:
+        result.append(i)
+    if max_cnt < tmp:
+        max_cnt = tmp
+        result = []
+        result.append(i)
+print(*result)
