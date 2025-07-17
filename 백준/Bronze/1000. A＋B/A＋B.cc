e8641ef4cdc9ce879c150884438216819483331c
서민재 <110338700+Vak-kas@users.noreply.github.com>
2025-07-17 13:17:21 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdint.h>
+
+using namespace std;
+
+int main(void){
+    uint32_t a, b;
+
+    cin >> a >> b;
+
+    uint32_t c  = a+b;
+
+    cout << a+b << endl;
+
+    return 0;
+}
